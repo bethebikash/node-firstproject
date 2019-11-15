7e67490a3991652ba100a4ceb722859506f0dccb
bethebikash <bhattaraibikash55@gmail.com>
2019-11-15 08:43:25 +0545

    express, middleware

--- index.js
+++ index.js
@@ -1,20 +1,69 @@
-var http = require('http');
+// var http = require('http');
 
-function cb(req, res){
-    res.statusCode = 200;
-    res.end("Hello form the application");
-}
+// function cb(req, res){
+//     res.statusCode = 200;
+//     res.end("Hello form the application");
+// }
 
-function cb2(req, res){
-    res.statusCode = 200;
-    res.end("Hello form second app");
-}
+// function cb2(req, res){
+//     res.statusCode = 200;
+//     res.end("Hello form second app");
+// }
 
-var app = http.createServer(cb);
-app.listen(3000);
+// var app = http.createServer(cb);
+// app.listen(3000);
 
-var app2 = http.createServer(cb2);
-app2.listen(3001);
+// var app2 = http.createServer(cb2);
+// app2.listen(3001);
+
+
+
+
+// express
+
+// const express = require('express');
+
+// const app = express()
+
+
+// app.get('/registration', function(req, res) {
+//     console.log('in registration');
+//     var x = {name: "Bikash"}
+//     res.status(200)
+//     res.set({
+//         'Content-Type': 'application/json'
+//     })
+//     res.send('hello')
+// })
+// app.listen(3002);
+
+// const express = require('express');
+
+// const app = express()
+
+
+// middleware
+
+const express = require('express');
+
+const app = express()
+
+app.get('/booking', function(req, res, next) {
+    console.log('in first middleware do something');
+    next();
+},
+
+function(req, res, next){
+    console.log('in the second middleware');
+    var x = {name: "Bikash"}
+    res.status(200)
+    res.set({
+        'Content-Type': 'application/json'
+    })
+    res.send('hello')
+})
+app.listen(3003);
+
 
 
 
